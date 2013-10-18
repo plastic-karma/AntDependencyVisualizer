1e0e4d6ab37b523ac2895ea0484c485cd782d055
plastic-karma <benjaminrogge@Benjamins-MacBook-Pro.local>
2013-10-18 20:48:55 +0200

    addes comments + minor changes

--- src/main/scala/antgraphvisualizer/DependencyCalculator.scala
+++ src/main/scala/antgraphvisualizer/DependencyCalculator.scala
@@ -9,44 +9,71 @@ import scala.xml.Node
 
 object DependencyCalculator {
   
-  class NodeExtension(node: Node) {
+  /**
+   * Extension class for Node for some convenience methods.
+   */
+  implicit class NodeExtension(node: Node) {
+    
+    /**
+     * Gets the attribute value of the attribute with the given name.
+     * Returns empty Option if attribute does not exist
+     */
     def getAttributeValue(attributeName: String): Option[String] = {
       val attribute = node \ ("@" + attributeName)
       if (attribute.isEmpty) None
       else Option(attribute.head.text)
     }
     
+    /**
+     * Returns true iff an attribute with the given name exists in the node.
+     */
     def hasAttribute(attributeName: String): Boolean = {
       return !(node \ ("@" + attributeName) isEmpty)
     }
   }
-  implicit def nodeCoercer(node: Node) = new NodeExtension(node)
   
+  /**
+   * Gets the dependencies of the given target in the given build file. The result is a map in the form of target -> (dependency1,..,dependencyn), 
+   * whereas only direct dependencies for a target are listed, however for every dependent target, there is a  distinct entry in the result map.
+   */
   def getDependencies(buildFileResolver: String => NodeSeq, buildFileName: String, targetName:String): Map[String, List[String]] = {
      
+    /**
+     * Maps a Sequence of Nodes to map where the keys are coming from the name attribute and the values are coming from the value attribute 
+     */
+    def getProperties(nodes: NodeSeq): Map[String, String] = {
+      nodes.foldLeft(Map[String, String]())((map, node) => map + ((node.getAttributeValue("name").get -> node.getAttributeValue("value").get)))
+    }
+    
+    
+    lazy val PROPERTY_PATTERN = "\\$\\{(.+)\\}".r
+    
+    /**
+     * Replaces property usages with the values.
+     */
+    def replaceProperties(input: String, properties: Map[String, String]): String = {
+	  PROPERTY_PATTERN.replaceAllIn(input, m => properties.getOrElse(m.group(1), ""))
+	}
+    
 	def getAllTargetNodesAndProperties(buildFileName: String, properties: Map[String, String]): (NodeSeq, Map[String, String]) = {
 	  val antFile = buildFileResolver(buildFileName) 
-	  val properties = (antFile \ "property").filter(node => node.hasAttribute("name") && node.hasAttribute("value")).
-	  											foldLeft(Map[String, String]())(
-	  											    (map, node) => map + ((node.getAttributeValue("name").get -> node.getAttributeValue("value").get)))
+	  val properties = getProperties((antFile \ "property").filter(node => node.hasAttribute("name") && node.hasAttribute("value")))
+	  											
 	  
 	  (antFile \ "import").foldLeft[(NodeSeq, Map[String, String])]((antFile \ "target", properties))(
 			  					(p, node) => {
 			  					  val (ns, properties) = p
-			  					  val (newTargets, newProperties) = 
-			  					    getAllTargetNodesAndProperties(
+			  					  val (newTargets, newProperties) =  getAllTargetNodesAndProperties(
 			  					        replaceProperties(node.getAttributeValue("file").get, properties), 
-			  					        properties
-		  					        )
+			  					        properties)
 			  					  (newTargets ++ ns, newProperties ++ properties)
-			  					})
+			  					}
+		  					)
 	}
 	
-	def replaceProperties(input: String, properties: Map[String, String]): String = {
-	  val pattern = "\\$\\{(.+)\\}".r
-	  pattern.replaceAllIn(input, m => properties.getOrElse(m.group(1), ""))
-	}
-	
+	/**
+	 * Interal recursive function to calculate dependencies.
+	 */
 	def getDependenciesInternal(targetName: String, targets: NodeSeq): Map[String, List[String]] = {
 	  val target = targets.find(currentTarget => currentTarget.attributes.exists(md => md.key == "name" && md.value.text == targetName))
 	  assert(target.nonEmpty)
