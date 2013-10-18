5405103e0e5c9ad8cb760193cb693b113590cb5b
plastic-karma <benjaminrogge@Benjamins-MacBook-Pro.local>
2013-10-18 17:57:02 +0200

    added import tests

---
+++ src/test/scala/antgraphvisualizer/DependencyCalculatorImportSpec.scala
@@ -0,0 +1,55 @@
+package antgraphvisualizer
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+import scala.xml.NodeSeq
+
+class DependencyCalculatorImportSpec extends FlatSpec with Matchers {
+
+  def buildFileRepository(buildFiles: Map[String, NodeSeq])(buildFileName: String) = buildFiles(buildFileName)
+  
+  "T1 in B1 depends on T2 in B2 and T2 depends on T3 in B2" should "produce T1 -> (T2), T2 -> (T3)" in {
+	  	val buildFiles = Map(
+	  	    "B1" -> 
+	  	              <project>
+	  	    			<import file="B2" />
+        				<target name="T1" depends="T2"/>
+	  	    		  </project>,
+    		"B2" ->
+		             <project>
+	  	    		   <target name="T2" depends="T3"/>
+	  	    		   <target name="T3"/>
+	  	    		 </project>
+  			)
+        DependencyCalculator.getDependencies(buildFileRepository(buildFiles), "B1", "T1") should equal (Map("T1" -> List("T2"), "T2" -> List("T3")))
+  }
+  
+  "T1 in B1 depends on T2 in B2 and T3 in B1. T2 depends on T4 in B3. T3 depends on T5 in B3" should 
+  "T1 -> (T2, T3), T2 -> (T4), T3 -> (T5)" in {
+  	  	val buildFiles = Map(
+	  	    "B1" -> 
+	  	              <project>
+	  	    			<import file="B2" />
+	  	    			<import file="B3" />
+        				<target name="T1" depends="T2, T3"/>
+	  	    			<target name="T3" depends="T5"/>
+	  	    		  </project>,
+    		"B2" ->
+		             <project>
+	  	    		   <import file="B3" />
+	  	    		   <target name="T2" depends="T4"/>
+	  	    		 </project>,
+    		"B3" ->
+		              <project>
+	  	    		    <target name="T4"/>
+	  	    		    <target name="T5"/>
+	  	    		 </project>
+  			)
+        DependencyCalculator.getDependencies(buildFileRepository(buildFiles), "B1", "T1") should equal (
+            Map("T1" -> List("T3", "T2"), "T2" -> List("T4"), "T3" -> List("T5")))
+  }
+  
+  
+  
+  
+}
