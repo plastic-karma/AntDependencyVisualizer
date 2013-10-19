cb090005e962d7f300b325e502c381e5e9f2eba1
plastic-karma <benjaminrogge@Benjamins-MacBook-Pro.local>
2013-10-19 03:36:48 +0200

    main uses commandline arguments

--- src/main/scala/antgraphvisualizer/Main.scala
+++ src/main/scala/antgraphvisualizer/Main.scala
@@ -8,39 +8,43 @@ import com.mxgraph.swing.mxGraphComponent
 import com.mxgraph.layout.mxIGraphLayout
 import com.mxgraph.layout.mxFastOrganicLayout
 import com.mxgraph.layout.hierarchical.mxHierarchicalLayout
+import com.mxgraph.layout.mxCircleLayout
+import com.mxgraph.layout.mxOrganicLayout
+import com.mxgraph.layout.mxStackLayout
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-//    val dependencies = DependencyCalculator.getDependencies(
-//    									    str => scala.xml.XML.loadFile(new File("/Users/benjaminrogge/workspace-export/Build", str)),
-//  											"build_posy_batch.xml",
-//  											"build_all_posy_batch"
-  											val dependencies = DependencyCalculator.getDependencies(
-  													str => scala.xml.XML.loadFile(new File("./testfiles", str)),
-  													"build1.xml",
-  													"T1"
-	)
+    val startTimeDependencies = System.currentTimeMillis()
+    val dependencies = DependencyCalculator.getDependencies(
+        str => scala.xml.XML.loadFile(new File(args(0), str)),
+        args(1),
+        args(2))
+    val endTimeDependencies = System.currentTimeMillis()
   	val graph = antgraphvisualizer.AntJGraphAdapter.generateJGraph(dependencies)
-
- 
- 
-    //val graphLayout = new JGraphSimpleLayout(JGraphSimpleLayout.TYPE_TILT, 400, 400)
-
-  
+  	val endTimeGraph = System.currentTimeMillis()
+  	val graphLayout = new mxHierarchicalLayout(graph);
+    graph.getModel().beginUpdate();
+    graphLayout.execute(graph.getDefaultParent());
+    graph.getModel().endUpdate()
+    val endTimeLayout = System.currentTimeMillis()
+    
+    println("Dependency time: " + (endTimeDependencies - startTimeDependencies) / 1000.0)
+    println("mxGraph    time: " + (endTimeGraph - endTimeDependencies) / 1000.0)
+    println("layout     time: " + (endTimeLayout - endTimeGraph) / 1000.0)
+    
+    
+    
+    
     new SampleGraph(graph)                          
   }
   
   class SampleGraph(graph: mxGraph) extends JFrame {
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-	
-        val graphLayout = new mxHierarchicalLayout(graph);
+  	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
 
-        // layout using morphing
-        graph.getModel().beginUpdate();
-        graphLayout.execute(graph.getDefaultParent());
-        graph.getModel().endUpdate()
-	
+
+
+
 	val graphComponent = new mxGraphComponent(graph);
 	getContentPane().add(new JScrollPane(graphComponent))
 	pack()
