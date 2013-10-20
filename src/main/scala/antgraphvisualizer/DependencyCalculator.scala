b6bb9ece30e31cc5556442e282405c743f2ad04f
plastic-karma <benjaminrogge@Benjamins-MacBook-Pro.local>
2013-10-20 14:50:31 +0200

    imports are now only visited once

--- src/main/scala/antgraphvisualizer/DependencyCalculator.scala
+++ src/main/scala/antgraphvisualizer/DependencyCalculator.scala
@@ -2,7 +2,6 @@ package antgraphvisualizer
 
 import scala.language.implicitConversions
 import scala.language.postfixOps
-
 import scala.xml.NodeSeq
 import java.io.File
 import scala.xml.Node
@@ -45,7 +44,6 @@ object DependencyCalculator {
       nodes.foldLeft(Map[String, String]())((map, node) => map + ((node.getAttributeValue("name").get -> node.getAttributeValue("value").get)))
     }
     
-    
     lazy val PROPERTY_PATTERN = "\\$\\{(.+)\\}".r
     
     /**
@@ -54,27 +52,63 @@ object DependencyCalculator {
     def replaceProperties(input: String, properties: Map[String, String]): String = {
 	  PROPERTY_PATTERN.replaceAllIn(input, m => properties.getOrElse(m.group(1), ""))
 	}
+   
     
-	def getAllTargetNodesAndProperties(buildFileName: String, properties: Map[String, String]): (NodeSeq, Map[String, String]) = {
-	  val antFile = buildFileResolver(buildFileName) 
-	  val properties = getProperties((antFile \ "property").filter(node => node.hasAttribute("name") && node.hasAttribute("value")))
-	  											
-	  
-	  (antFile \ "import").foldLeft[(NodeSeq, Map[String, String])]((antFile \ "target", properties))(
-			  					(p, node) => {
-			  					  val (ns, properties) = p
-			  					  val (newTargets, newProperties) =  getAllTargetNodesAndProperties(
-			  					        replaceProperties(node.getAttributeValue("file").get, properties), 
-			  					        properties)
-			  					  (newTargets ++ ns, newProperties ++ properties)
-			  					}
-		  					)
-	}
+    
+    /**
+     * Returns all target nodes from a given file and it's imports.
+     */
+    def getAllTargetNodes(buildFileName: String): NodeSeq = {
+      
+        /**
+         * Auxiliary function to calculate target nodes from a given file and it's imports.
+         * @param buildFileName The build file to get the targets from.
+         * @param Auxiliary parameter to keep track of ant properties.
+         * @param visitedImports Auxiliary parameter to keep track of visited imports, 
+         * so every file is only visited once.
+         */
+		def getAllTargetNodesAndPropertiesInternal(
+		    buildFileName: String, 
+		    existingProperties: Map[String, String], 
+		    visitedImports: List[String])
+		: (NodeSeq, Map[String, String], List[String]) = {
+		  
+	      if (visitedImports.contains(buildFileName)) (NodeSeq.Empty, Map(), visitedImports)
+	      else {
+	    	  val allVisitedImports = buildFileName :: visitedImports
+			  val antFile = buildFileResolver(buildFileName) 
+			  val allExistingProperties = 
+			    getProperties((antFile \ "property").filter(node => node.hasAttribute("name") && node.hasAttribute("value"))) ++ existingProperties
+			  
+			  
+			  (antFile \ "import").foldLeft[(NodeSeq, Map[String, String], List[String])]((antFile \ "target", allExistingProperties, allVisitedImports))(
+					  (p, node) => {
+						  val (ns, currentProperties, currentVisitedImports) = p
+								  val (newTargets, newProperties, newVisitedImports) =  getAllTargetNodesAndPropertiesInternal(
+										  replaceProperties(node.getAttributeValue("file").get, currentProperties), 
+										  currentProperties, currentVisitedImports)
+										  (newTargets ++ ns, newProperties ++ currentProperties, newVisitedImports)
+					  }
+					  )
+	      }
+		}
+		getAllTargetNodesAndPropertiesInternal(buildFileName, Map(), List())._1
+    }
+   
 	
 	/**
 	 * Interal recursive function to calculate dependencies.
+	 * @param targetName The name of the target to calculate dependencies for.
+	 * @param targets All available ant targets to calculate dependencies from.
+	 * @param visitedNodes Auxiliary parameter to keep track of already visited targets, 
+	 * so dependencies get for every target get only calculated once. 
 	 */
-	def getDependenciesInternal(targetName: String, targets: NodeSeq, visitedNodes: List[String]): Map[String, List[String]] = {
+	def getDependenciesInternal(
+	    targetName: String, 
+	    targets: NodeSeq, 
+	    visitedNodes: List[String])
+	: Map[String, List[String]] = {
+	  
 	  if (visitedNodes.contains(targetName)) Map()
 	  else {
 		  val target = targets.find(currentTarget => currentTarget.attributes.exists(md => md.key == "name" && md.value.text == targetName))
@@ -83,17 +117,18 @@ object DependencyCalculator {
 		  if (directDepends isDefined) {
 		    val directDependsList = directDepends.get.split(",").map(str => str.trim)
 		    directDependsList.foldLeft[Map[String, List[String]]](Map())((map, str) =>
-		      map // old Map
-		       + (targetName -> (str :: map.getOrElse(targetName, List()))) // The entry of targetName + currentDepend
-		      ++ getDependenciesInternal(str, targets, visitedNodes ++ map.keys)) // Recursive dependencies of currentDepend
+		      map
+		       + (targetName -> (str :: map.getOrElse(targetName, List())))
+		      ++ getDependenciesInternal(str, targets, visitedNodes ++ map.keys))
 		  }
 		  else Map()
 	  }
 	}
-	val startTime = System.currentTimeMillis()
-	val targets = getAllTargetNodesAndProperties(buildFileName, Map())._1
-	val endTime = System.currentTimeMillis()
-	println ("targets " + ((endTime - startTime) / 1000.0))
-    getDependenciesInternal(targetName, targets, List())
+	
+	
+	/*
+	 * Actual entry point for getDependencies
+	 */
+    getDependenciesInternal(targetName, getAllTargetNodes(buildFileName), List())
   }
 }
